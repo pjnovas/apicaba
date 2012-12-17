09a0f4a78cdbda1fa46801f6df83253decc54de3
Pablo Novas <pjnovas@gmail.com>
2012-12-16 21:56:40 -0300

    improved tests for formatted

--- test/formatter.js
+++ test/formatter.js
@@ -1,7 +1,8 @@
 var expect = require('expect.js')
   , Formatter = require('../models/formatter.js')
   , Stream = require('stream').Stream
-  , config = require(__dirname + '/config/test.json');
+  , config = require(__dirname + '/config/test.json')
+  , sinon = require('sinon');
 
 describe('Formatter', function(){
 
@@ -12,8 +13,46 @@ describe('Formatter', function(){
     expect(formatter).to.be.a(Stream);
   });
 
-  describe('#fetch()', function(){
+  describe('#write()', function(){
 
+    it('should be able to format csv data', function(done){
+      var csvDummyData = "pepe, loco";
+      var formatter = new Formatter('csv');
+
+      var spy_formatCSV = sinon.stub(formatter, "formatCSV", function(csv){
+        expect(csv).to.be.equal(csvDummyData);
+        spy_formatCSV.restore();
+        done();
+      });
+
+      formatter.write(csvDummyData);
+    });
+
+    it('should be able to format xml data', function(done){
+      var xmlDummyData = "<node>pepe loco</node>";
+      var formatter = new Formatter('xml');
+
+      var spy_formatXML = sinon.stub(formatter, "formatXML", function(csv){
+        expect(csv).to.be.equal(xmlDummyData);
+        spy_formatXML.restore();
+        done();
+      });
+
+      formatter.write(xmlDummyData);
+    });
+
+    it('should be able to pass the exact same data if no format is specify', function(done){
+      var noFormaDummyData = "hello formatter";
+      var formatter = new Formatter();
+
+      formatter.on('data', function(data){
+        expect(data).to.be.a('string');
+        expect(data).to.be.equal(noFormaDummyData);
+        done();
+      });
+
+      formatter.write(noFormaDummyData);
+    });
   });
 
   describe('events', function(){
